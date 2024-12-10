2c91f87dac544963fd071e1dd1c2e8580f9ee76b
parsehex <1779937+parsehex@users.noreply.github.com>
2024-12-10 14:50:48 -0500

    add setup, update scripts

---
+++ setup-repo.ps1
@@ -0,0 +1,33 @@
+#!/usr/bin/env pwsh
+
+# is nvm installed?
+if (-not (Get-Command nvm -ErrorAction SilentlyContinue)) {
+	Write-Output "nvm could not be found"
+	exit
+}
+
+# is node v12 installed?
+$nvmList = nvm list
+if ($nvmList -notmatch "v12") {
+	Write-Output "node is not v12"
+	exit
+}
+
+git pull --recurse-submodules
+
+# node_modules?
+if (-Not (Test-Path -Path "node_modules")) {
+	npm install
+}
+
+if (-Not (Test-Path -Path "games.json")) {
+	Copy-Item -Path "games.json.example" -Destination "games.json"
+	Write-Output "games.json created - please run the following to edit:"
+	Write-Output "notepad games.json"
+}
+
+# have had to run these separate on windows
+node src/update-version
+node src/update-games-data
+npx --yes patch-package
+npx --yes webpack
---
+++ setup-repo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# is nvm installed?
+if ! command -v nvm &> /dev/null; then
+	echo "nvm could not be found"
+	exit 1
+fi
+
+# is node v12 installed?
+if ! nvm list | grep -q "v12"; then
+	echo "node is not v12"
+	exit 1
+fi
+
+git pull --recurse-submodules
+
+# node_modules?
+if [ ! -d "node_modules" ]; then
+	npm install
+fi
+
+if [ ! -f "games.json" ]; then
+	cp "games.json.example" "games.json"
+	echo "games.json created - please run the following to edit:"
+	echo "notepad games.json"
+fi
+
+npm run build
